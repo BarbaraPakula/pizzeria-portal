4c4f6ec1d4eea6802c4577100f311ff2c40b91d2
Barbara Pakula <barbara.pakula@hotmail.com>
2021-05-05 10:56:16 +0200

    Add routing, pages and subpages views

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 import React from 'react';
+import MainLayout from '../src/components/layout/MainLayout/MainLayout';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Login from '../src/components/views/Login/Login';
+import Homepage from '../src/components/views/Homepage/Homepage';
+import Kitchen from './components/views/Kitchen/Kitchen';
+import Waiter from './components/views/Waiter/Waiter';
+import Tables from './components/views/Tables/Tables';
+import BookingInfo from './components/views/BookingInfo/BookingInfo';
+import BookingNew from './components/views/BookingNew/BookingNew';
+import EventsInfo from './components/views/EventsInfo/EventsInfo';
+import EventsNew from './components/views/EventsNew/EventsNew';
+import OrderInfo from './components/views/OrderInfo/OrderInfo';
+import OrderNew from './components/views/OrderNew/OrderNew';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          lalalalalallaalalla
-        </a>
-      </header>
-    </div>
+
+    <BrowserRouter basename={'/panel'}>
+      <MainLayout>
+        <Switch>
+          <Route exact path={`${process.env.PUBLIC_URL}/`} component={Homepage} />
+          <Route exact path={`${process.env.PUBLIC_URL}/login`} component={Login} />
+          <Route exact path={`${process.env.PUBLIC_URL}/tables`} component={Tables} />
+          <Route exact path={`${process.env.PUBLIC_URL}/tables/booking/book/:id`} component={BookingInfo} />
+          <Route exact path={`${process.env.PUBLIC_URL}/tables/booking/new`} component={BookingNew} />
+          <Route exact path={`${process.env.PUBLIC_URL}/tables/events/event/:id`} component={EventsInfo} />
+          <Route exact path={`${process.env.PUBLIC_URL}/tables/events/new`} component={EventsNew} />
+          <Route exact path={`${process.env.PUBLIC_URL}//waiter/ordering/order/:id`} component={OrderInfo} />
+          <Route exact path={`${process.env.PUBLIC_URL}/waiter/order/new`} component={OrderNew} />
+          <Route exact path={`${process.env.PUBLIC_URL}/waiter`} component={Waiter} />
+          <Route exact path={`${process.env.PUBLIC_URL}/Kitchen`} component={Kitchen} />
+
+        </Switch>
+      </MainLayout>
+    </BrowserRouter>
   );
 }
 
